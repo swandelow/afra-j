5cb21c6bd9c5cdf467a732abdb827e1b2a251712
swandelow <swandelow@despegar.com>
2015-10-11 02:33:58 -0300

    reset de archivos de config

--- source/comandos/reset_all.sh
+++ source/comandos/reset_all.sh
@@ -10,5 +10,7 @@ rm -r "$grupo/sospechosas"
 rm -r "$grupo/log"
 rm -r "$grupo/mae"
 rm -r "$grupo/reportes"
+rm "$grupo/conf/AFINSTAL.cnfg" 
+rm "$grupo/conf/AFINSTAL.lg" 
 
 echo Se ha eliminado todos los directrios creadas por AFINSTALL
