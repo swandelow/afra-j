db60db4dc1af041b066746f7d86ddf17907df004
swandelow <swandelow@despegar.com>
2015-10-11 00:22:51 -0300

    pequeña utilidad para resetear el ambiente

---
+++ source/comandos/reset_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+grupo=./grupo06
+
+rm -r "$grupo/bin"
+rm -r "$grupo/aceptadas"
+rm -r "$grupo/novedades"
+rm -r "$grupo/rechazadas"
+rm -r "$grupo/sospechosas"
+rm -r "$grupo/log"
+rm -r "$grupo/mae"
+rm -r "$grupo/reportes"
+
+echo Se ha eliminado todos los directrios creadas por AFINSTALL
